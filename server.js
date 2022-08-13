beb5c66e0a2e73810e3b910d34231a868d048342
adhamaly <adhamaly126@gmail.com>
2022-08-13 21:05:43 +0200

    test

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ require("./loaders/index")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.info(`Listening on PORT ${PORT}`);
+  console.info(`Listening on PORT ${PORT} ...`);
 });
